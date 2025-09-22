28848f39a559616c9bbb8a18545a90ba931de9b7
Z-Moore548 <zmoore1@nait.ca>
2025-09-22 12:24:18 -0600

    Started working on Dice game AI

--- DiceGameAI/Assets/AITemplate.cs
+++ DiceGameAI/Assets/AITemplate.cs
@@ -1,6 +1,6 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+
 
 // Your AI will need to access the GameManager script on the object in the scene.
 // You will have access to the following methods:
@@ -16,9 +16,12 @@ using UnityEngine.UI;
 public class AITemplate : MonoBehaviour
 {
     [SerializeField] GameManager gameManager;
-
+    [SerializeField] Button aiButton;
     [SerializeField] AIStates currentState = AIStates.RollDice1;
+    [SerializeField] int[] diceValues = new int[5];
+    [SerializeField] int[] diceCount = new int[6];
 
+    [SerializeField] int onePair, twoPair, threeKind, fourKind, fullHouse, smallStraight, largeStraight;
     enum AIStates
     {
         RollDice1,
@@ -31,7 +34,11 @@ public class AITemplate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!gameManager.IsRolling() && aiButton.interactable == false)
+        {
+            CheckCombos();
+            aiButton.interactable = true;
+        }
     }
 
     public void TakeAIStep()
@@ -40,54 +47,24 @@ public class AITemplate : MonoBehaviour
         {
             case AIStates.RollDice1:
                 gameManager.RollDice(); //Push the roll the dice button.
-                currentState = AIStates.EvaluateDice1;
+                aiButton.interactable = false;
+                for (int i = 0; i < 6; i++)
+                {
+                    gameManager.SetComboActive(i, false);
+                }
+                currentState = AIStates.EvaluateDice1; // not going to have each on diffrent button presses. on one press it should roll, eval, and keep
                 break;
             case AIStates.EvaluateDice1:
-                Debug.Log("Gamers");
-                                                // Count and store in an array how many of each value there are.
-                                                // If you have 5 numbers in a sequence & haven’t completed LS, select LS 
-                                                // Else If you have 4 of a single num & haven’t completed it FK, select FK 
-                                                // Else If you have 3 of a single num and 2 of a different num & haven’t completed it FH, select FH 
-
-                                                // Else If LS is done, and you have 4 numbers in sequence & haven’t completed SS, select SS 
-                                                // Else If FK and FH are done, and you have 3 of a single num & haven’t completed TK, select TK 
-                                                // Else if LS, SS, FK, FH, and TK are done, and you have 2 of a single num and 2 of a different num & haven’t completed TP, select TP 
-                                                // If a combo was selected, 
-                                                // check if all combos are complete.
-                                                // 	End game
-                                                // If not  
-                                                //  End turn                
-
+                CheckCombos();
                 break;
             case AIStates.KeepDice:
-                                                // If SS or LS is not done, check the array for three or more numbers in sequence.
-                                                // 	If there is, keep those numbers (go next)
-                                                // If FK, FH, TK, and TP are done, check for 2 or more number is sequence.
-                                                // 	If there is, keep those numbers (go next)
-                                                // If SS and LS are done, or there aren’t three numbers in sequence. Check the array for any numbers where there is more than one.
-                                                // 	If there are multiple duplicate numbers. Check if TP and FH is done
-                                                // 		If they are not done, keep both pairs (go next)
-                                                // 		If they are done keep one set of duplicates, however many they may be. (go next)
-                                                // 	If only one set of duplicates
-                                                // 		Keep that set of duplicates (go next)
-
+                                                
                 break;
             case AIStates.RollDice2:
-                                                //Push the roll the dice button
+                                                
                 break;
             case AIStates.EvaluateDice2:
-                                                // Count and store in an array how many of each value there are.
-                                                // If you have 5 numbers in a sequence & haven’t completed LS, select LS 
-                                                // Else If you have 4 numbers in a sequence & haven’t completed SS, select SS 
-                                                // Else If you have 4 of a single num & haven’t completed FK, select FK 
-                                                // Else If you have 3 of a single num and 2 of a different num & haven’t completed it FH, select FH 
-                                                // Else If you have 3 of a single num & haven’t completed TK, select TK 
-                                                // Else If you have 2 of a single num and 2 of a different num & haven’t completed TP, select TP 
-                                                // check if all combos are complete.
-                                                // End game
-                                                // Else 
-                                                // End turn
-
+                                                
                 break;
             default:
                 break;
@@ -96,7 +73,70 @@ public class AITemplate : MonoBehaviour
 
     void CheckCombos()
     {
-        
+        //initialize combo varibales
+        onePair = -1;
+        twoPair = -1;
+        threeKind = -1;
+        fourKind = -1;
+        fullHouse = -1;
+        smallStraight = -1;
+        largeStraight = -1;
+
+        //get dice values
+        gameManager.GetDiceValues(ref diceValues);
+
+        //reset dice array
+        for (int i = 0; i < diceCount.Length; i++)
+        {
+            diceCount[i] = 0;
+        }
+        //count the dice
+        for (int i = 0; i < diceValues.Length; i++)
+        {
+            diceCount[diceValues[i]]++;
+        }
+        //evaluate dice values to check for active combos
+        for (int i = 0; i < diceCount.Length; i++)
+        {
+            if (diceCount[i] >= 2)
+            {
+                if (onePair == -1)
+                {
+                    onePair = i;
+                }
+                else
+                {
+                    twoPair = i;
+                }
+            }
+            if (diceCount[i] >= 3)
+            {
+                threeKind = i;
+            }
+            if (diceCount[i] >= 4)
+            {
+                fourKind = i;
+            }
+            if (twoPair != -1 && threeKind != -1)
+            {
+                fullHouse = 1;
+            }
+            if (i < 3) //this works to find the straights but there is probaby a better way to do this.
+            {
+                if (diceCount[i] >= 1 && diceCount[i + 1] >= 1 && diceCount[i + 2] >= 1 && diceCount[i + 3] >= 1)
+                {
+                    smallStraight = i;
+                }
+            }
+            if (i < 2)
+            {
+                if (diceCount[i] >= 1 && diceCount[i + 1] >= 1 && diceCount[i + 2] >= 1 && diceCount[i + 3] >= 1 && diceCount[i + 4] >= 1)
+                {
+                    largeStraight = i;
+                }
+            }
+            
+        }
     }
 
     void UpdateComboButtons()

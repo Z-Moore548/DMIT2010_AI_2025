a5232c12f5f6bbae3479db09d0abc6884bd2057b
Z-Moore548 <zmoore1@nait.ca>
2025-10-16 14:29:55 -0600

    Contextual movement how detects gaps and turns or jumps gaps. also turns and alins with angled walls

--- AutoMoveObject/Assets/Scipts/Mover.cs
+++ AutoMoveObject/Assets/Scipts/Mover.cs
@@ -1,11 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class AIMover : MonoBehaviour
 {
+    [SerializeField] GameObject dropCheck, jumpCheck;
+    [SerializeField] Rigidbody rb;
     [SerializeField] float movementSpeed;
     RaycastHit hitFront, hitLeft, hitRight;
     [SerializeField] float forwardDist, sideDist;
-    bool leftWall, rightWall;
+    [SerializeField] float jumpForce;
+    bool leftWall, rightWall, jumping;
+    int randInt;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,70 +18,90 @@ public class AIMover : MonoBehaviour
         movementSpeed = 10.0f;
         forwardDist = 1.0f;
         sideDist = 2.0f;
-
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (Physics.BoxCast(transform.position + new Vector3(0, 1, 0), new Vector3(0.5f, 1, 0.5f), transform.forward, out hitFront, Quaternion.identity, forwardDist))
+        if (Physics.BoxCast(transform.position + new Vector3(0, 1, 0), new Vector3(0.5f, 0.9f, 0.5f), transform.forward, out hitFront, Quaternion.identity, forwardDist))
         {
-            if (Physics.BoxCast(transform.position + new Vector3(0, 1, 0), new Vector3(0.5f, 1, 0.5f), -transform.right, out hitLeft, Quaternion.identity, sideDist))
-            {
-
-                leftWall = true;
-
-            }
-
-            if (Physics.BoxCast(transform.position + new Vector3(0, 1, 0), new Vector3(0.5f, 1, 0.5f), transform.right, out hitRight, Quaternion.identity, sideDist))
-            {
-
-                rightWall = true;
-
-            }
-            
-            if (leftWall && !rightWall)
-            {
-
-                transform.Rotate(Vector3.up, 90);
-            }
-            else if (!leftWall && rightWall)
-            {
-
-                transform.Rotate(Vector3.up, -90);
-            }
-            else if (leftWall && rightWall)
-            {
-
-                transform.Rotate(Vector3.up, 180);
-            }
-            else
-            {
-                transform.Rotate(Vector3.up, 90);
-            }
-
+            transform.LookAt(transform.position - hitFront.normal);
+            RotateAway();
         }
-        leftWall = false;
-        rightWall = false;
-    }
+        if(transform.position.y < -0.4)//this wont work if the floor is varying heights
+        {
+            if(!Physics.BoxCast(dropCheck.transform.position, new Vector3(0.5f, 0.9f, 0.5f), -transform.up, out hitFront, Quaternion.identity, forwardDist))
+            {
+                if (Physics.BoxCast(jumpCheck.transform.position, new Vector3(0.5f, 0.9f, 0.5f), -transform.up, out hitFront, Quaternion.identity, forwardDist))
+                {
+                    Jump();
+                }
+                else
+                {
+                    RotateAway();
+                }
+            }
+        }
+        
 
+    }
     void FixedUpdate()
     {
         transform.Translate(Vector3.forward * movementSpeed * Time.fixedDeltaTime);
     }
 
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    if (collision.transform.tag == "Wall")
-    //    {
-    //        transform.Rotate(Vector3.up, 90);
-    //    }
-    //}
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.transform.tag == "Wall")
-    //    {
-    //        transform.Rotate(Vector3.up, 90);
-    //    }
-    //}
+    void RotateAway()
+    {
+        if (Physics.BoxCast(transform.position + new Vector3(0, 1, 0), new Vector3(0.5f, 0.9f, 0.5f), -transform.right, out hitLeft, Quaternion.identity, sideDist))
+        {
+
+            leftWall = true;
+
+        }
+
+        if (Physics.BoxCast(transform.position + new Vector3(0, 1, 0), new Vector3(0.5f, 0.9f, 0.5f), transform.right, out hitRight, Quaternion.identity, sideDist))
+        {
+
+            rightWall = true;
+
+        }
+
+        if (leftWall && !rightWall)
+        {
+
+            transform.Rotate(Vector3.up, 90);
+        }
+        else if (!leftWall && rightWall)
+        {
+
+            transform.Rotate(Vector3.up, -90);
+        }
+        else if (leftWall && rightWall)
+        {
+
+            transform.Rotate(Vector3.up, 180);
+        }
+        else
+        {
+            randInt = Random.Range(0, 2);
+            if (randInt == 0)
+            {
+                transform.Rotate(Vector3.up, 90);
+                Debug.Log("RightRand");
+            }
+            else
+            {
+                transform.Rotate(Vector3.up, -90);
+                Debug.Log("LeftRand");
+            }
+        }
+
+
+        leftWall = false;
+        rightWall = false;
+    }
+    void Jump()
+    {
+        rb.AddForce(new Vector3(0, 1, 0) * jumpForce * Time.fixedDeltaTime, ForceMode.Impulse);
+    }
 }

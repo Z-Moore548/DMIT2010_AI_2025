e164bb99146090896b2f7b878507f7cf0057a3c9
Z-Moore548 <zmoore1@nait.ca>
2025-11-17 10:38:43 -0700

    tried to get teh security guaurds chasing. failed

--- Pathfinding/Assets/Scripts/AIPathfinder.cs
+++ Pathfinding/Assets/Scripts/AIPathfinder.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class AIPathfinder : MonoBehaviour
 {
     [SerializeField] GameObject startNode, endNode, currentNode, targetNode, prevNode;
+    [SerializeField] GameObject chaseTarget;
     [SerializeField] GameObject[] waypoints;
     float moveSpeed;
     int waypointIndex = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,59 +16,73 @@ public class AIPathfinder : MonoBehaviour
         targetNode = currentNode;
         endNode = waypoints[waypointIndex];
         moveSpeed = 5.0f;
+        chaseTarget = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //If the AI is at the target node than find a new target to move to
-        if (Vector3.Distance(transform.position, targetNode.transform.position) < 0.1f)
+        if(chaseTarget != null)
         {
-            prevNode = currentNode;
-            currentNode = targetNode;
-
-            if(currentNode == endNode)
-            {
-                waypointIndex = Random.Range(0, waypoints.Length);
-                // if (waypointIndex > waypoints.Length)
-                // {
-                //     waypointIndex = 0;
-                // }
-                endNode = waypoints[waypointIndex];
-            }
-
-            float closestDistance = 10000;
-            //GameObject closestNode;
-
-            Pathnode pathScript = currentNode.GetComponent<Pathnode>();
-
-            if (pathScript != null)
-            {
-                //bool found = false;
-                //int pathIndex = 0;
-
-                //int randNum = Random.Range(0, pathScript.connections.Count);
-                //targetNode = pathScript.connections[randNum];
-
-                for (int i = 0; i < pathScript.connections.Count; i++)
-                {
-                    if(pathScript.connections[i] != prevNode && pathScript.connections[i].GetComponent<Pathnode>().nodeActive)
-                    {
-                        if(Vector3.Distance(pathScript.connections[i].transform.position, endNode.transform.position) < closestDistance)
-                        {
-                            targetNode = pathScript.connections[i];
-                            closestDistance = Vector3.Distance(pathScript.connections[i].transform.position, endNode.transform.position);
-                        }
-                        
-                    }
-                }
-                
-                
-            }
+            Debug.Log("Chasing");
         }
-        else
+        if(chaseTarget == null)
         {
-            transform.Translate((targetNode.transform.position - transform.position).normalized * moveSpeed * Time.deltaTime);
+            //If the AI is at the target node than find a new target to move to
+            if (Vector3.Distance(transform.position, targetNode.transform.position) < 0.1f)
+            {
+                prevNode = currentNode;
+                currentNode = targetNode;
+
+                if(currentNode == endNode)
+                {
+                    waypointIndex = Random.Range(0, waypoints.Length);
+                    // if (waypointIndex > waypoints.Length)
+                    // {
+                    //     waypointIndex = 0;
+                    // }
+                    endNode = waypoints[waypointIndex];
+                }
+
+                float closestDistance = 10000;
+                //GameObject closestNode;
+
+                Pathnode pathScript = currentNode.GetComponent<Pathnode>();
+
+                if (pathScript != null)
+                {
+                    //bool found = false;
+                    //int pathIndex = 0;
+
+                    //int randNum = Random.Range(0, pathScript.connections.Count);
+                    //targetNode = pathScript.connections[randNum];
+
+                    for (int i = 0; i < pathScript.connections.Count; i++)
+                    {
+                        if(pathScript.connections[i] != prevNode && pathScript.connections[i].GetComponent<Pathnode>().nodeActive)
+                        {
+                            if(Vector3.Distance(pathScript.connections[i].transform.position, endNode.transform.position) < closestDistance)
+                            {
+                                targetNode = pathScript.connections[i];
+                                closestDistance = Vector3.Distance(pathScript.connections[i].transform.position, endNode.transform.position);
+                            }
+                            
+                        }
+                    }
+                    
+                    
+                }
+            }
+            else
+            {
+                transform.Translate((targetNode.transform.position - transform.position).normalized * moveSpeed * Time.deltaTime);
+            }
         }
     }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("F");
+        chaseTarget = other.gameObject;
+    }
+    
 }
